aa78464abd2c30732a5ddac12aaa4d9df70f2416
moud <haibou177@gmail.com>
2023-10-22 15:43:00 +0000

    done

--- src/ContactUs.js
+++ src/ContactUs.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function ContactUs() {
   const [name, setName] = useState('');
@@ -6,11 +6,22 @@ function ContactUs() {
   const [phone, setPhone] = useState('');
   const [phoneType, setPhoneType] = useState('');
   const [comments, setComments] = useState('');
+  const [validationErrors, setValidationErrors] = useState([]);
+  const [hasSubmitted, setHasSubmitted] = useState(false);
+
+  useEffect(() => {
+    let errors = [];
+    if (name.length === 0) errors.push("Please enter your name");
+    if (!email.includes("@")) errors.push("Please provide a valid email");
+    setValidationErrors(errors);
+  }, [name, email]);
 
   const onSubmit = e => {
     // Prevent the default form behavior so the page doesn't reload.
     e.preventDefault();
 
+    setHasSubmitted(true);
+    if (validationErrors.length > 0) return alert('Cannot Submit.')
     // Create a new object for the contact us information.
     const contactUsInformation = {
       name,
@@ -31,12 +42,22 @@ function ContactUs() {
     setPhone('');
     setPhoneType('');
     setComments('');
+    setValidationErrors([]);
+    setHasSubmitted(false);
   }
 
   return (
     <div>
       <h2>Contact Us</h2>
       <form onSubmit={onSubmit}>
+        { validationErrors.length > 0 && hasSubmitted && (
+          <div>
+            The following errors were found:
+            <ul>
+              {validationErrors.map((error, i) => <li key={i}>{error}</li>)}
+            </ul>
+          </div>
+        )}
         <div>
           <label htmlFor='name'>Name:</label>
           <input
